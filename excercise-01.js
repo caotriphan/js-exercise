03aab0f50d006de4ee55c7d1fc6cc59ca51fe8e7
Tri Phan <phancaotri2212@gmail.com>
2023-06-15 14:35:42 +0700

    complete excercise-01

---
+++ excercise-01.js
@@ -0,0 +1,7 @@
+const getFirstValue = [1, 2, 3];
+const getFirstValue1 = [80, 5, 100];
+const getFirstValue2 = [-500, 0, 50];
+
+console.log(getFirstValue.shift()); // 1
+console.log(getFirstValue1.shift()); // 80
+console.log(getFirstValue2.shift()); // -500
