7cc9483b2403205327f44684d90ea7cc1504ac4f
Quang Phan <quang.phan@tallygroup.com.au>
2023-06-20 10:25:34 +0700

    setup jest test

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-import * as lib from './index'
+const lib = require('./index')
 
-// test
+test('additional should work', () => {
+  expect(lib.addition(1)).toBe(2);
+  expect(lib.addition(2)).toBe(3);
+  expect(lib.addition(10)).toBe(11);
+});
